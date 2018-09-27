2fbd82a2823fcdcce6b8287ee99cf18f79c6a7a3
Juliette Monsel <j_4321@protonmail.com>
2018-09-27 10:58:08 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,5 +22,4 @@ setup(name = "FolderSync",
 	                ("/usr/share/pixmaps", ["foldersync.svg"]),
 					("/usr/share/applications", ["foldersync.desktop"])],
 	  scripts = ["foldersync"],
-	  long_description = """FolderSync is a utility to keep your backups up to date. You can visualise the differences between your data and your backup and choose what will be copied and what will be suppressed. The frequent synchronisation paths can be saved as favorites.""",
-	  install_requires = ["psutil"])
+	  long_description = """FolderSync is a utility to keep your backups up to date. You can visualise the differences between your data and your backup and choose what will be copied and what will be suppressed. The frequent synchronisation paths can be saved as favorites.""")
