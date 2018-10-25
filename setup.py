96a059890354659cc75cc78516bc524f7ac26d4a
Juliette Monsel <j_4321@protonmail.com>
2018-10-26 00:19:39 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -2,24 +2,32 @@
 # -*- coding:Utf-8 -*-
 
 from setuptools import setup
+from sys import platform
 
 with open('foldersynclib/version.py') as file:
     exec(file.read())
 
-files = ["images/*"]
-
-setup(name = "FolderSync",
-	  version = __version__,
-	  description = "Folder synchronisation software",
-	  author = "Juliette Monsel",
-	  author_email = "j_4321@protonmail.fr",
-	  url = "https://github.com/j4321/FolderSync",
-	  license = "GNU General Public License v3",
-	  packages = ['foldersynclib'],
-	  package_data = {'foldersynclib' : files },
-	  data_files = [("share/locale/en_US/LC_MESSAGES/", ["foldersynclib/locale/en_US/LC_MESSAGES/FolderSync.mo"]),
-					("share/locale/fr_FR/LC_MESSAGES/", ["foldersynclib/locale/fr_FR/LC_MESSAGES/FolderSync.mo"]),
-	                ("/usr/share/pixmaps", ["foldersync.svg"]),
-					("/usr/share/applications", ["foldersync.desktop"])],
-	  scripts = ["foldersync"],
-	  long_description = """FolderSync is a utility to keep your backups up to date. You can visualise the differences between your data and your backup and choose what will be copied and what will be suppressed. The frequent synchronisation paths can be saved as favorites.""")
+if platform.startswith('linux'):
+    files = ["images/*"]
+    data_files = [("/usr/share/locale/en_US/LC_MESSAGES/", ["foldersynclib/locale/en_US/LC_MESSAGES/FolderSync.mo"]),
+                  ("/usr/share/locale/fr_FR/LC_MESSAGES/", ["foldersynclib/locale/fr_FR/LC_MESSAGES/FolderSync.mo"]),
+                  ("/usr/share/doc/foldersync/", ["README.rst", "changelog"]),
+                  ("/usr/share/man/man1", ["foldersync.1.gz"]),
+                  ("/usr/share/pixmaps", ["foldersync.svg"]),
+                  ("/usr/share/applications", ["foldersync.desktop"])]
+else:
+    files = ["images/*", "locale/en_US/LC_MESSAGES/*", "locale/fr_FR/LC_MESSAGES/*"]
+    data_files = []
+    
+setup(name="FolderSync",
+	  version=__version__,
+	  description="Folder synchronisation software",
+	  author="Juliette Monsel",
+	  author_email="j_4321@protonmail.fr",
+	  url="https://github.com/j4321/FolderSync",
+	  license="GNU General Public License v3",
+	  packages=['foldersynclib'],
+	  package_data={'foldersynclib' : files},
+	  data_files=data_files,
+	  scripts=["foldersync"],
+	  long_description="""FolderSync is a utility to keep your backups up to date. You can visualise the differences between your data and your backup and choose what will be copied and what will be suppressed. The frequent synchronisation paths can be saved as favorites.""")
